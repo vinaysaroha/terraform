905aa931180d10ecedfc813a00edf421cd538fb8
vinaysaroha <vinayrhcsacertified@gmail.com>
2023-01-14 18:17:04 +0530

    removing chapter9

--- chapter8 implementing AZ fixing data source and for each meta argument/output.tf
+++
@@ -1,7 +0,0 @@
-output "public_ip_using_list" {
-  value = toset([for instance in aws_instance.instance1: instance.public_ip])
-}
-
-output "public_ip_using_map" {
-  value = tomap({for az, instance in aws_instance.instance1: az => instance.public_ip})
-}
